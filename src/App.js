56d5358416e25ff5c2cec82f69d9a8f7194d204b
Amit Patel <amitpatel_7@msn.com>
2018-07-26 00:10:22 -0700

    8/8 tests passed

--- src/App.js
+++ src/App.js
@@ -4,59 +4,70 @@ import './App.css';
 class Drumpad extends Component {
   constructor(props) {
     super(props);
+    this.handleOnClick = this.handleOnClick.bind(this);
+
+    this.state = {
+      played: ''
+    }
+  }
+
+  handleOnClick(event) {
+    this.setState({
+      played: event.target.getAttribute("id")
+    });
   }
 
   render() {
     return (
-      <div className="Pad">
-        <button className="drumpad" id="music-1">
-          Q
-          <audio src="#" className="clip" id="Q"></audio>
-        </button>
-        <button className="drumpad" id="music-2">
-          W
-          <audio src="#" className="clip" id="W"></audio>
-        </button>
-        <button className="drumpad" id="music-3">
-          E
-          <audio src="#" className="clip" id="E"></audio>        
-        </button>
-        <button className="drumpad" id="music-4">
-          A
-          <audio src="#" className="clip" id="A"></audio>        
-        </button>
-        <button className="drumpad" id="music-5">
-          S
-          <audio src="#" className="clip" id="S"></audio> 
-        </button>
-        <button className="drumpad" id="music-6">
-          D
-          <audio src="#" className="clip" id="D"></audio>
-        </button>
-        <button className="drumpad" id="music-7">
-          Z
-          <audio src="#" className="clip" id="Z"></audio>
-        </button>
-        <button className="drumpad" id="music-8">
-          X
-          <audio src="#" className="clip" id="X"></audio>
-        </button>
-        <button className="drumpad" id="music-9">
-          C
-          <audio src="#" className="clip" id="C"></audio>
-        </button>
+      <div className="container">
+        <div id="display">{this.state.played}</div>
+        <div className="Pad">
+          <button className="drum-pad" id="music-1" onClick={ this.handleOnClick }>
+            Q
+            <audio src="#" className="clip" id="Q"></audio>
+          </button>
+          <button className="drum-pad" id="music-2" onClick={ this.handleOnClick }>
+            W
+            <audio src="#" className="clip" id="W"></audio>
+          </button>
+          <button className="drum-pad" id="music-3" onClick={ this.handleOnClick }>
+            E
+            <audio src="#" className="clip" id="E"></audio>        
+          </button>
+          <button className="drum-pad" id="music-4" onClick={ this.handleOnClick }>
+            A
+            <audio src="#" className="clip" id="A"></audio>        
+          </button>
+          <button className="drum-pad" id="music-5" onClick={ this.handleOnClick }>
+            S
+            <audio src="#" className="clip" id="S"></audio> 
+          </button>
+          <button className="drum-pad" id="music-6" onClick={ this.handleOnClick }>
+            D
+            <audio src="#" className="clip" id="D"></audio>
+          </button>
+          <button className="drum-pad" id="music-7" onClick={ this.handleOnClick }>
+            Z
+            <audio src="#" className="clip" id="Z"></audio>
+          </button>
+          <button className="drum-pad" id="music-8" onClick={ this.handleOnClick }>
+            X
+            <audio src="#" className="clip" id="X"></audio>
+          </button>
+          <button className="drum-pad" id="music-9" onClick={ this.handleOnClick }>
+            C
+            <audio src="#" className="clip" id="C"></audio>
+          </button>
+        </div>      
       </div>
     );
   }
 };
 
-const Display = () => <div id="display"></div>
-
 class App extends Component {
   render() {
     return (
       <div className="App" id="drum-machine">
-        <Display />
         <Drumpad />
       </div>
     );
